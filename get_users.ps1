4421679d17c850ebccd08310c9d2ce797fa65fff
Glassinatorn <gusten1995@gmail.com>
2025-04-10 00:14:45 +0200

    added a menu for the script

--- get_users.ps1
+++ get_users.ps1
@@ -1,13 +1,40 @@
+function Show-Menu {
+    param (
+        [string]$Title = 'Menu'
+    )
+    Write-Host "================ $Title ================"
+    Write-Host "1: Get users from desktops"
+    Write-Host "2: Get users from servers"
+    Write-Host "q: Exit"
+    Write-Host "========================================="
+}
+
 # getting servers to check
 $content = Get-Content servers.secret | ConvertFrom-Json
 
 $desktops = $content.Desktops
 $servers = $content.Servers
 
-# getting users
-foreach ($desktop in $desktops) {
-    $users = Get-WmiObject -computername $desktop -class Win32_UserAccount -filter "LocalAccount=True" | select PSComputername, Name, Status
+do {
+    Show-Menu
 
-    Write-Output $users
-}
+    $selection = Read-Host "Please make a selection"
+    switch ($selection) {
+        '1' {
+            Clear-Host
+            foreach ($desktop in $desktops) {
 
+                $users = Get-WmiObject -computername $desktop -class Win32_UserAccount -filter "LocalAccount=True" | select PSComputername, Name, Status
+
+                Write-Output $users
+            }
+        } '2' {
+            Clear-Host
+            foreach ($server in $servers) {
+                $users = Get-WmiObject -computername $server -class Win32_UserAccount -filter "LocalAccount=True" | select PSComputername, Name, Status
+
+                Write-Output $users
+            }
+        }
+    }
+} until ($selection -eq 'q')
